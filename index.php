e60b578e73029cde22acea81f8ee4384e567429c
Linda Castillo <lindacastillor@gmail.com>
2020-06-09 09:46:16 -0500

    index.php: estructura para home, actualizar hojas de .sass

--- index.php
+++ index.php
@@ -1,3 +1,122 @@
 <?php include('header.php'); ?>
 
+<section class="pt-100 pb-100">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<p class="txt48">bloque de hero-img con gradient</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="bg-beige pt-md-5 pt-4 pb-md-6 pb-4">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h2 class="txt36 pb-md-5 pb-3"><strong>NUESTRO <br class="d-md-block d-none"><span class="c-green">CATÁLOGO</span></strong></h2>
+				<a href="#" class="has-btn bg-dark-gray">DESCARGAR</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- PRODUCTOS DESTACADOS -->
+<section class="bg-white pt-md-6 pt-4 pb-md-6 pb-4">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h3 class="txt36 pb-md-6 pb-3 text-uppercase"><strong>PRODUCTOS DESTACADOS</strong></h3>
+			</div>
+			<div class="col-md-3 col-sm-6 col-12">
+				<a href="#">
+					<div class="img-card-square mb-3" style="background-image: url('img/producto-verde1.png');"></div>
+					<h2 class="txt18 text-uppercase c-dark-gray"><strong>Silla Guaymas Lounge</strong></h2>
+					<p class="txt12 c-dark-gray">Silla</p>
+				</a>
+			</div>
+			<div class="col-md-3 col-sm-6 col-12">
+				<a href="#">
+					<div class="img-card-square mb-3" style="background-image: url('img/producto-verde1.png');"></div>
+					<h2 class="txt18 text-uppercase c-dark-gray"><strong>Silla Guaymas Lounge</strong></h2>
+					<p class="txt12 c-dark-gray">Silla</p>
+				</a>
+			</div>
+			<div class="col-md-3 col-sm-6 col-12">
+				<a href="#">
+					<div class="img-card-square mb-3" style="background-image: url('img/producto-verde1.png');"></div>
+					<h2 class="txt18 text-uppercase c-dark-gray"><strong>Silla Guaymas Lounge</strong></h2>
+					<p class="txt12 c-dark-gray">Silla</p>
+				</a>
+			</div>
+			<div class="col-md-3 col-sm-6 col-12">
+				<a href="#">
+					<div class="img-card-square mb-3" style="background-image: url('img/producto-verde1.png');"></div>
+					<h2 class="txt18 text-uppercase c-dark-gray"><strong>Silla Guaymas Lounge</strong></h2>
+					<p class="txt12 c-dark-gray">Silla</p>
+				</a>
+			</div>
+			<div class="col-auto ml-auto mr-auto pt-md-6 pt-4">
+				<a href="#" class="has-btn bg-green">VER TODOS LOS PRODUCTOS</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- PROYECTOS RECIENTES -->
+<section class="bg-green pt-md-6 pt-4 pb-md-6 pb-4">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h3 class="c-white txt36 pb-md-6 pb-3 text-uppercase"><strong>Proyectos recientes</strong></h3>
+			</div>
+			<div class="col-md-4 col-sm-6 col-12 mb-4">
+				<a href="#">
+					<div class="img-card-rectangle mb-3" style="background-image: url('img/producto-verde1.png');"></div>
+					<h2 class="txt18 text-uppercase c-white"><strong>Silla Guaymas Lounge</strong></h2>
+					<p class="txt12 c-white">Silla</p>
+				</a>
+			</div>
+			<div class="col-md-4 col-sm-6 col-12 mb-4">
+				<a href="#">
+					<div class="img-card-rectangle mb-3" style="background-image: url('img/producto-verde1.png');"></div>
+					<h2 class="txt18 text-uppercase c-white"><strong>Silla Guaymas Lounge</strong></h2>
+					<p class="txt12 c-white">Silla</p>
+				</a>
+			</div>
+			<div class="col-md-4 col-sm-6 col-12 mb-4">
+				<a href="#">
+					<div class="img-card-rectangle mb-3" style="background-image: url('img/producto-verde1.png');"></div>
+					<h2 class="txt18 text-uppercase c-white"><strong>Silla Guaymas Lounge</strong></h2>
+					<p class="txt12 c-white">Silla</p>
+				</a>
+			</div>
+			<div class="col-md-4 col-sm-6 col-12 mb-4">
+				<a href="#">
+					<div class="img-card-rectangle mb-3" style="background-image: url('img/producto-verde1.png');"></div>
+					<h2 class="txt18 text-uppercase c-white"><strong>Silla Guaymas Lounge</strong></h2>
+					<p class="txt12 c-white">Silla</p>
+				</a>
+			</div>
+			<div class="col-md-4 col-sm-6 col-12 mb-4">
+				<a href="#">
+					<div class="img-card-rectangle mb-3" style="background-image: url('img/producto-verde1.png');"></div>
+					<h2 class="txt18 text-uppercase c-white"><strong>Silla Guaymas Lounge</strong></h2>
+					<p class="txt12 c-white">Silla</p>
+				</a>
+			</div>
+			<div class="col-md-4 col-sm-6 col-12 mb-4">
+				<a href="#">
+					<div class="img-card-rectangle mb-3" style="background-image: url('img/producto-verde1.png');"></div>
+					<h2 class="txt18 text-uppercase c-white"><strong>Silla Guaymas Lounge</strong></h2>
+					<p class="txt12 c-white">Silla</p>
+				</a>
+			</div>
+			<div class="col-auto ml-auto mr-auto pt-md-6 pt-4">
+				<a href="#" class="has-btn bg-beige c-dark-gray">VER TODOS LOS PROYECTOS</a>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?php include('footer.php'); ?>
