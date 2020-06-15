63de9db1783ede5301deb7b2a59a07784aeed4af
Brandon Espinosa <brandon.eg.yeoman@gmail.com>
2020-06-15 12:16:10 -0500

    Familias.php: Lista de familias en proceso

---
+++ familias.php
@@ -0,0 +1,52 @@
+<?php include('header.php'); ?>
+
+<!-- FAMILIAS -->
+<section>
+    <!-- FIXED BAR -->
+    <div class="container bg-white pt-3 pb-3">
+		<div class="row">
+			<div class="col-12 d-flex tab-menu">
+				<li class="active"><a href="#tab-1" class="has-btn btn-border-black text-uppercase">2019</a></li>
+				<li><a href="#tab-2" class="has-btn btn-border-black text-uppercase">2018</a></li>
+				<li><a href="#tab-3" class="has-btn btn-border-black text-uppercase">2017</a></li>
+				<li><a href="#tab-4" class="has-btn btn-border-black text-uppercase">2016</a></li>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12 bg-img vh100 pl-5 pr-5 pb-100 d-flex align-items-end" style="background-image: url('img/reyes1.jpg');">
+				<div class="mask-gradient-dark-gray"></div>
+				<div class="col-8 pl-5 pr-5 fade-in show delay4">
+					<h1 class="txt100 c-white is-bold">BIENVENIDO A LOS PATRONES</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+    <div class="container-fluid">
+		<div class="row">
+			<div class="col-12 bg-img vh100 pl-5 pr-5 pb-100 d-flex align-items-end" style="background-image: url('img/reyes1.jpg');">
+				<div class="mask-gradient-dark-gray"></div>
+				<div class="col-8 pl-5 pr-5 fade-in show delay4">
+					<h1 class="txt100 c-white is-bold">BIENVENIDO A LOS PATRONES</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- FIXED BAR -->
+<!--<section class="bg-white fixed-top pt-3 pb-3">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 d-flex tab-menu">
+				<li class="active"><a href="#tab-1" class="has-btn btn-border-black text-uppercase">2019</a></li>
+				<li><a href="#tab-2" class="has-btn btn-border-black text-uppercase">2018</a></li>
+				<li><a href="#tab-3" class="has-btn btn-border-black text-uppercase">2017</a></li>
+				<li><a href="#tab-4" class="has-btn btn-border-black text-uppercase">2016</a></li>
+			</div>
+		</div>
+	</div>
+</section>-->
+
+<?php include('footer.php'); ?>
