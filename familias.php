e0f92c049194688e4acb633fdb7203d299bc92fc
Brandon Espinosa <brandon.eg.yeoman@gmail.com>
2020-06-15 14:02:00 -0500

    familias.php: Lista de familias en proceso. Se necesita arreglar barra de filtros

--- familias.php
+++ familias.php
@@ -6,29 +6,44 @@
     <div class="container bg-white pt-3 pb-3">
 		<div class="row">
 			<div class="col-12 d-flex tab-menu">
-				<li class="active"><a href="#tab-1" class="has-btn btn-border-black text-uppercase">2019</a></li>
-				<li><a href="#tab-2" class="has-btn btn-border-black text-uppercase">2018</a></li>
-				<li><a href="#tab-3" class="has-btn btn-border-black text-uppercase">2017</a></li>
-				<li><a href="#tab-4" class="has-btn btn-border-black text-uppercase">2016</a></li>
+                <li><a href="#tab-1" class="has-btn bg-green text-uppercase">Descargar catálogo</a></li>
+				<li class="active"><a href="#tab-2" class="has-btn btn-border-black text-uppercase">2019</a></li>
+				<li><a href="#tab-3" class="has-btn btn-border-black text-uppercase">2018</a></li>
+				<li><a href="#tab-4" class="has-btn btn-border-black text-uppercase">2017</a></li>
+				<li><a href="#tab-5" class="has-btn btn-border-black text-uppercase">2016</a></li>
 			</div>
 		</div>
 	</div>
 	<div class="container-fluid">
-		<div class="row">
-			<div class="col-12 bg-img vh100 pl-5 pr-5 pb-100 d-flex align-items-end" style="background-image: url('img/reyes1.jpg');">
+		<div class="row odd-even-list">
+			<div class="col-12 bg-img vh100 pl-5 pr-5 d-flex align-items-center odd-even-element" style="background-image: url('img/reyes1.jpg');">
 				<div class="mask-gradient-dark-gray"></div>
-				<div class="col-8 pl-5 pr-5 fade-in show delay4">
-					<h1 class="txt100 c-white is-bold">BIENVENIDO A LOS PATRONES</h1>
+				<div class="pl-5 pr-5 fade-in show delay4">
+					<h1 class="txt100 c-white is-bold">REYES</h1>
 				</div>
 			</div>
 		</div>
-	</div>
-    <div class="container-fluid">
-		<div class="row">
-			<div class="col-12 bg-img vh100 pl-5 pr-5 pb-100 d-flex align-items-end" style="background-image: url('img/reyes1.jpg');">
+		<div class="row odd-even-list">
+			<div class="col-12 bg-img vh100 pl-5 pr-5 d-flex align-items-center odd-even-element" style="background-image: url('img/reyes1.jpg');">
 				<div class="mask-gradient-dark-gray"></div>
-				<div class="col-8 pl-5 pr-5 fade-in show delay4">
-					<h1 class="txt100 c-white is-bold">BIENVENIDO A LOS PATRONES</h1>
+				<div class="pl-5 pr-5 fade-in show delay4">
+					<h1 class="txt100 c-white is-bold">GUAYMAS</h1>
+				</div>
+			</div>
+		</div>
+        <div class="row odd-even-list">
+			<div class="col-12 bg-img vh100 pl-5 pr-5 d-flex align-items-center odd-even-element" style="background-image: url('img/reyes1.jpg');">
+				<div class="mask-gradient-dark-gray"></div>
+				<div class="pl-5 pr-5 fade-in show delay4">
+					<h1 class="txt100 c-white is-bold">NORESTENSE</h1>
+				</div>
+			</div>
+		</div>
+        <div class="row odd-even-list">
+			<div class="col-12 bg-img vh100 pl-5 pr-5 d-flex align-items-center odd-even-element" style="background-image: url('img/reyes1.jpg');">
+				<div class="mask-gradient-dark-gray"></div>
+				<div class="pl-5 pr-5 fade-in show delay4">
+					<h1 class="txt100 c-white is-bold">LÁZARO</h1>
 				</div>
 			</div>
 		</div>
